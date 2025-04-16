346433f66c4e1ca2a177f9626256bf0c0ca1d2cc
RFBiazon <rfbiazon@gmail.com>
2025-04-16 12:47:18 -0300

    Atualização das credenciais do Supabase

--- scripts/create-users.ts
+++ scripts/create-users.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://qjqjqjqjqjqjqjqjqjqj.supabase.co"
+const supabaseUrl = "https://osemdcmygzkrccaaqyvj.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcWpxanFqcWpxanFqcWpxanFqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mzk0OTMyOCwiZXhwIjoyMDU5NTI1MzI4fQ.BD1jvnLWleSA74_oF2sOH0Ux2A10YHg-Gy8UDlOOgI0"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
